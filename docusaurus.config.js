9d080be4d30f81d469c6c4a9f71530740cd7e0ca
ysz <1521627754@qq.com>
2023-03-12 23:41:04 +0800

    o

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yisuabzi', // Usually your GitHub org/user name.
+  projectName: 'maojunjie.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
